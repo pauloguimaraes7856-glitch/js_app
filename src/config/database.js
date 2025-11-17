fc7740a1d6b66490baf0bdb9cbb7af2702455656
ls <paulo-rogerio.guimaraes-filho@epita.fr>
2025-11-17 21:39:31 +0100

    Frontend + fixed controllers

--- src/config/database.js
+++ src/config/database.js
@@ -4,33 +4,29 @@ import fs from "fs";
 
 let db = null;
 
-
 export const getDb = () => db;
 
 export const initializeDatabase = async () => {
     const dbPath = config.databaseUrl;
-
 
     const folder = dbPath.substring(0, dbPath.lastIndexOf("/"));
     if (!fs.existsSync(folder)) {
         fs.mkdirSync(folder, { recursive: true });
     }
 
-
     db = new Database(dbPath);
     console.log("📁 Database initialized at:", dbPath);
-
 
     const User = (await import("../models/User.js")).default;
     const Car = (await import("../models/Car.js")).default;
 
-
-    User.createTable();
     Car.createTable();
+    User.createTable();
 
+
+    Car.seed();
 
     User.seed();
-    Car.seed();
 };
 
 export default db;
